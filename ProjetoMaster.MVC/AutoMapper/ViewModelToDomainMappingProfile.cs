03970a54a53c5f886e2d705e1127954a226f1cba
jhoninoni <jhoni_jk@hotmail.com>
2016-10-11 15:54:57 -0300

    Add files via upload

---
+++ ProjetoMaster.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using ProjetoMaster.Domain.Entities;
+using ProjetoMaster.MVC.ViewsModel;
+
+namespace ProjetoMaster.MVC.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        protected override void Configure()
+        {
+            //Mapper.CreateMap<ClienteViewModel, Cliente>();
+            Mapper.CreateMap<PessoaModelView, PessoaDomain>();
+        }
+    }
+}
