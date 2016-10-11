5b8455478b6cf1e9b973a3e4875feb217fd03101
jhoninoni <jhoni_jk@hotmail.com>
2016-10-11 15:44:06 -0300

    RodaBase

---
+++ RodarBaseDados/PainelBancoDados.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace RodarBaseDados
+{
+    partial class PainelBancoDados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCriaBaseDados = new System.Windows.Forms.Button();
+            this.btnRodaMassaDados = new System.Windows.Forms.Button();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.btnDroparBanco = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCriaBaseDados
+            // 
+            this.btnCriaBaseDados.Location = new System.Drawing.Point(29, 59);
+            this.btnCriaBaseDados.Name = "btnCriaBaseDados";
+            this.btnCriaBaseDados.Size = new System.Drawing.Size(224, 23);
+            this.btnCriaBaseDados.TabIndex = 0;
+            this.btnCriaBaseDados.Text = "Criar Base Dados";
+            this.btnCriaBaseDados.UseVisualStyleBackColor = true;
+            this.btnCriaBaseDados.Click += new System.EventHandler(this.btnCriaBaseDados_Click);
+            // 
+            // btnRodaMassaDados
+            // 
+            this.btnRodaMassaDados.Location = new System.Drawing.Point(29, 88);
+            this.btnRodaMassaDados.Name = "btnRodaMassaDados";
+            this.btnRodaMassaDados.Size = new System.Drawing.Size(224, 23);
+            this.btnRodaMassaDados.TabIndex = 1;
+            this.btnRodaMassaDados.Text = "Rodar Massa Dados";
+            this.btnRodaMassaDados.UseVisualStyleBackColor = true;
+            this.btnRodaMassaDados.Click += new System.EventHandler(this.btnRodaMassaDados_Click);
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.Location = new System.Drawing.Point(35, 186);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(0, 25);
+            this.lblMensagem.TabIndex = 2;
+            // 
+            // btnDroparBanco
+            // 
+            this.btnDroparBanco.Location = new System.Drawing.Point(29, 30);
+            this.btnDroparBanco.Name = "btnDroparBanco";
+            this.btnDroparBanco.Size = new System.Drawing.Size(224, 23);
+            this.btnDroparBanco.TabIndex = 3;
+            this.btnDroparBanco.Text = "Deletar Banco Atual";
+            this.btnDroparBanco.UseVisualStyleBackColor = true;
+            this.btnDroparBanco.Click += new System.EventHandler(this.btnDroparBanco_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnDroparBanco);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.btnRodaMassaDados);
+            this.Controls.Add(this.btnCriaBaseDados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCriaBaseDados;
+        private System.Windows.Forms.Button btnRodaMassaDados;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Button btnDroparBanco;
+    }
+}
+
---
+++ RodarBaseDados/PainelBancoDados.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+using ProjetoMaster.Domain.Entities;
+using ProjetoMaster.Infra.CrossCuting.Criptografar;
+using ProjetoMaster.Infra.Data.Contexto;
+using ProjetoMaster.Infra.Data.Repositories;
+using RodarBaseDados.Properties;
+
+namespace RodarBaseDados
+{
+    public partial class PainelBancoDados : Form
+    {
+        #region Declaracao de Variaveis
+
+        //private ClienteRepository _clienteRepository;
+        private UsuarioRepository _usuarioRepository;
+        private const string SenhaPadrao = "testeSenha101010";
+
+        #endregion
+
+        public PainelBancoDados()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDroparBanco_Click(object sender, EventArgs e)
+        {
+            //DeletarBase();
+        }
+
+        private void btnCriaBaseDados_Click(object sender, EventArgs e)
+        {
+            CriarBaseDados();
+        }
+
+        private void btnRodaMassaDados_Click(object sender, EventArgs e)
+        {
+            GeraMassaDados();
+        }
+
+        #region Metodos de Acao
+
+        private void DeletarBase()
+        {
+            var contexto = new ContextDb();
+
+            try
+            {
+                contexto.Database.Delete();
+                lblMensagem.Text = Resources.BaseDeletadaComSucesso;
+            }
+            catch (Exception ex)
+            {
+                lblMensagem.Text = Resources.ErroDeletarBase;
+            }
+        }
+
+        private void CriarBaseDados()
+        {
+            var contexto = new ContextDb();
+
+            try
+            {
+                contexto.Database.CreateIfNotExists();
+                lblMensagem.Text = Resources.MensagemBaseCriadaComSucesso;
+            }
+            catch (Exception ex)
+            {
+                lblMensagem.Text = Resources.MensagemErroCriarBase;
+            }
+        }
+
+        private void GeraMassaDados()
+        {
+            var contexto = new ContextDb();
+
+            try
+            {
+                contexto.Database.CreateIfNotExists();
+
+                CriarMassaDadosProduto();
+                CriarMassaDadosCliente();
+                CriarMassaUsuario();
+
+                lblMensagem.Text = Resources.MassaDadosMapeadaSucesso;
+            }
+            catch (Exception ex)
+            {
+                lblMensagem.Text = Resources.ErroMapearMassaDados;
+            }
+        }
+
+        private void CriarMassaDadosProduto()
+        {
+            //var produto = new PessoaDomain() { Nome = "Cadeira"};
+            //var produto1 = new PessoaDomain() { Nome = "Mesa"};
+            //var produto2 = new PessoaDomain() { Nome = "Sofa" };
+
+            //_produtoRepository = new ProdutoRepository();
+            //_produtoRepository.Add(produto);
+            //_produtoRepository.Add(produto1);
+            //_produtoRepository.Add(produto2);
+        }
+
+        private void CriarMassaDadosCliente()
+        {
+            //var cliente = new Cliente()
+            //{
+            //    Nome = "Jhoni",
+            //    Sobrenome = "Oliveira",
+            //    Email = "Jhoni_jk@hotmail.com",
+            //    Ativo = true,
+            //    DataNascimento = new DateTime(),
+            //    ProdutoId = 1,
+            //    Produto = new Produto() { Nome = "Cadeira", Valor = 25 }
+            //};
+
+            //var cliente1 = new Cliente()
+            //{
+            //    Nome = "Jean", 
+            //    Sobrenome = "Oliveira", 
+            //    Email = "Jean@hotmail.com", 
+            //    Ativo = true,
+            //    DataNascimento = new DateTime(),
+            //    ProdutoId = 11,
+            //    Produto = new Produto() { Nome = "Mesa", Valor = 50 }
+            //};
+
+            //var cliente2 = new Cliente()
+            //{
+            //    Nome = "Nicolas", 
+            //    Sobrenome = "Oliveira", 
+            //    Email = "Nicolas@hotmail.com", 
+            //    Ativo = false,
+            //    DataNascimento = new DateTime(),
+            //    ProdutoId = 11,
+            //    Produto = new Produto() { Nome = "Sofa", Valor = 100 }
+            //};
+
+            //_clienteRepository = new ClienteRepository();
+            //_clienteRepository.Add(cliente);
+            //_clienteRepository.Add(cliente1);
+            //_clienteRepository.Add(cliente2);
+        }
+
+        private void CriarMassaUsuario()
+        {
+            //var usuario = new UsuarioDomain()
+            //{
+            //    Nome = "Jhoni de Oliveira Barros",
+            //    Cpf = 42053581888,
+            //    Email = "Jhoni_jk@hotmail.com",
+            //    DataPrimeiroAcesso = DateTime.Now,
+            //    DataUltimoAcesso = DateTime.Now,
+            //    PrimeiroAcesso = true,
+            //    Senha = new Criptografia().Encrypt(SenhaPadrao)
+            //};
+
+            _usuarioRepository = new UsuarioRepository();
+            //_usuarioRepository.Add(usuario);
+        }
+
+        #endregion
+    }
+}
