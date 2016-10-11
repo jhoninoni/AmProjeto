153b961385e2476218e04bb60cdbdae3f953e9c6
jhoninoni <jhoni_jk@hotmail.com>
2016-10-11 15:48:16 -0300

    Add files via upload

---
+++ ProjetoMaster.Service.DadosPulseiraApi/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace ProjetoMaster.Service.DadosPulseiraApi.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
